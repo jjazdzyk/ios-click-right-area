94b7a7508770865521593c94a944fc9b74f47448
Jakub Jażdżyk <jakub.jazdzyk@kissdigital.pl>
2018-07-16 14:25:37 +0200

    Initial commit.

---
+++ Click Right Area/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Right Area
+//
+//  Created by Jakub Jażdżyk on 16.07.2018.
+//  Copyright © 2018 Jakub Jażdżyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
